93d39be62176b127031ee5e1bbc50de2f05bf624
Jason Ephraim <jason.ephraim.novatex@gmail.com>
2020-05-01 20:51:52 -0500

    added first draft of one month of cost calc logic

--- js/main.js
+++ js/main.js
@@ -18,6 +18,15 @@ let usageSubmit = document.getElementById("usageSubmit");
 // Define plan list
 let planResults = document.getElementById("planResults");
 
+// add another event listener for click to grab inputs from date fields
+
+// for each plan in array
+// make rates for no entry (0), 0-500, 500-1000, 1000-2000, 200+ for each plan and then apply for each month with if statements
+// If UsageJan == 0, then JanCost is 0
+// elseif 0 < UsageJan < 500 then JanCost = UsageJan * Usage500to1000rate
+// do this for each tier and add up totals
+// sort array by lowest cost
+
 usageSubmit.addEventListener("click", function () {
   let proxyUrl = `https://cors-anywhere.herokuapp.com/`;
   let callAddress = `http://api.powertochoose.org/api/PowerToChoose/plans?zip_code=${userZIP.value}`;
@@ -25,16 +34,36 @@ usageSubmit.addEventListener("click", function () {
     .then((response) => response.json())
     .then((allPlans) => {
       console.log(allPlans);
-      let allPlansHTMLoutput = `<div class="card">
-                              <div class="card-body">
-                                  <h5 class="card-title">${allPlans.data[0].plan_name}</h5>
-                                  <h6 class="card-subtitle mb-2 text-muted">${allPlans.data[0].company_name}</h6>
-                                  <ul class="list-group list-group-flush">
-                                      <li class="list-group-item"><a href="${allPlans.data[0].fact_sheet}" target="_blank" rel="noopener noreferrer">Plan Fact Sheet</a></li>
-                                      
-                                  </ul>
-                              </div>
-                          </div>`;
-      planResults.innerHTML = allPlansHTMLoutput;
+      for (index = 0; index < allPlans.data.length; index++) {
+        let costJan = 0;
+        if (usageJan == "") {
+          costJan = 0;
+        } else if (usageJan <= 500) {
+          costJan =
+            ((usageJan * allPlans.data[0].price_kwh500) / 500 / 100) * usageJan; // usage in KwH times 500 Kwh cost (in cents) divided by 500 KwH = rate for usage in cents / 100 * usage = cost for usage in dollars
+        } else if (usageJan <= 1000) {
+          costJan =
+            ((usageJan * allPlans.data[0].price_kwh1000) / 1000 / 100) *
+            usageJan; // usage in KwH times 500 Kwh cost (in cents) divided by 500 KwH = rate for usage in cents / 100 * usage = cost for usage in dollars
+        } else {
+          costJan =
+            ((usageJan * allPlans.data[0].price_kwh2000) / 2000 / 100) *
+            usageJan; // usage in KwH times 500 Kwh cost (in cents) divided by 500 KwH = rate for usage in cents / 100 * usage = cost for usage in dollars
+        }
+
+        console.log(costJan);
+      }
+
+      //   let allPlansHTMLoutput = `<div class="card">
+      //                           <div class="card-body">
+      //                               <h5 class="card-title">${allPlans.data[0].plan_name}</h5>
+      //                               <h6 class="card-subtitle mb-2 text-muted">${allPlans.data[0].company_name}</h6>
+      //                               <ul class="list-group list-group-flush">
+      //                                   <li class="list-group-item"><a href="${allPlans.data[0].fact_sheet}" target="_blank" rel="noopener noreferrer">Plan Fact Sheet</a></li>
+
+      //                               </ul>
+      //                           </div>
+      //                       </div>`;
+      //   planResults.innerHTML = allPlansHTMLoutput;
     });
 });
