3171d0a663a0e0a48b9b39a10f11d44f09d2e75b
Josh Medeski <josh.medeski@gmail.com>
2020-05-03 15:10:47 -0500

    [wip] Add functions

--- js/main.js
+++ js/main.js
@@ -1,24 +1,39 @@
 // Define zip Code entry field
-let userZIP = document.getElementById("userZIP");
-// Define usage entry fields
-let usageJan = document.getElementById("usageJan");
-let usageFeb = document.getElementById("usageFeb");
-let usageMar = document.getElementById("usageMar");
-let usageApr = document.getElementById("usageApr");
-let usageMay = document.getElementById("usageMay");
-let usageJun = document.getElementById("usageJun");
-let usageJul = document.getElementById("usageJul");
-let usageAug = document.getElementById("usageAug");
-let usageSep = document.getElementById("usageSep");
-let usageOct = document.getElementById("usageOct");
-let usageNov = document.getElementById("usageNov");
-let usageDec = document.getElementById("usageDec");
+let userZipCodeValue = document.getElementById("userZIP").value;
 // Define submit button
 let usageSubmit = document.getElementById("usageSubmit");
 // Define plan list
 let planResults = document.getElementById("planResults");
 
 // add another event listener for click to grab inputs from date fields
+
+/**
+ * Calculates the cost per month.
+ *
+ * @param monthlyUsageCost The cost for the month give
+ * @param plan The plan you want to calculate the cost for
+ * @returns the cost per month
+ */
+function calcMonthCost(monthlyUsageCost, plan) {
+  if (monthlyUsageCost == 0) {
+    return 0;
+  } else if (monthlyUsageCost <= 500) {
+    // usage in KwH times 500 Kwh cost (in cents) divided by 500 KwH = rate for usage in cents / 100 * usage = cost for usage in dollars
+    return (
+      ((monthlyUsageCost * plan.price_kwh500) / 500 / 100) * monthlyUsageCost
+    );
+  } else if (monthlyUsageCost <= 1000) {
+    // usage in KwH times 500 Kwh cost (in cents) divided by 500 KwH = rate for usage in cents / 100 * usage = cost for usage in dollars
+    return (
+      ((monthlyUsageCost * plan.price_kwh1000) / 1000 / 100) * monthlyUsageCost
+    );
+  } else {
+    // usage in KwH times 500 Kwh cost (in cents) divided by 500 KwH = rate for usage in cents / 100 * usage = cost for usage in dollars
+    return (
+      ((monthlyUsageCost * plan.price_kwh2000) / 2000 / 100) * monthlyUsageCost
+    );
+  }
+}
 
 // for each plan in array
 // make rates for no entry (0), 0-500, 500-1000, 1000-2000, 200+ for each plan and then apply for each month with if statements
@@ -27,73 +42,52 @@ let planResults = document.getElementById("planResults");
 // do this for each tier and add up totals
 // sort array by lowest cost
 
-usageSubmit.addEventListener("click", function () {
-  //api call
+/**
+ * Gets the plans from the powertochoose API from the provided zipcode
+ *
+ * @param zipCode The zip code to get the plans for
+ * @returns a promise of the avaliable plans from the API
+ */
+function getPlans(zipcode) {
   let proxyUrl = `https://cors-anywhere.herokuapp.com/`;
-  let callAddress = `http://api.powertochoose.org/api/PowerToChoose/plans?zip_code=${userZIP.value}`;
-  fetch(proxyUrl + callAddress, { cache: "force-cache" })
-    .then((response) => response.json())
-    .then((allPlans) => {
-      //end api call
-      console.log(allPlans);
-      for (index = 0; index < allPlans.data.length; index++) {
-        //Jan
-        let months = [
-          "Jan",
-          "Feb",
-          "Mar",
-          "Apr",
-          "May",
-          "Jun",
-          "Jul",
-          "Aug",
-          "Sep",
-          "Oct",
-          "Nov",
-          "Dec",
-        ];
-        for (i = 0; i < months.length; i++) {
-          //iterate through each month in months
-          if (usageJan.value == 0) {
-            costJan = 0;
-          } else if (usageJan.value <= 500) {
-            costJan =
-              ((usageJan.value * allPlans.data[index].price_kwh500) /
-                500 /
-                100) *
-              usageJan.value; // usage in KwH times 500 Kwh cost (in cents) divided by 500 KwH = rate for usage in cents / 100 * usage = cost for usage in dollars
-          } else if (usageJan.value <= 1000) {
-            costJan =
-              ((usageJan.value * allPlans.data[index].price_kwh1000) /
-                1000 /
-                100) *
-              usageJan.value; // usage in KwH times 500 Kwh cost (in cents) divided by 500 KwH = rate for usage in cents / 100 * usage = cost for usage in dollars
-          } else {
-            costJan =
-              ((usageJan.value * allPlans.data[index].price_kwh2000) /
-                2000 /
-                100) *
-              usageJan.value; // usage in KwH times 500 Kwh cost (in cents) divided by 500 KwH = rate for usage in cents / 100 * usage = cost for usage in dollars
-          }
-        }
+  let callAddress = `http://api.powertochoose.org/api/PowerToChoose/plans?zip_code=${zipcode}`;
+  return fetch(proxyUrl + callAddress, {
+    cache: "force-cache",
+  }).then((response) => response.json());
+}
 
-        //end Jan
+function getUserMonthlyValues() {
+  return [
+    document.getElementById("usageJan").value,
+    document.getElementById("usageFeb").value,
+    document.getElementById("usageMar").value,
+    document.getElementById("usageApr").value,
+    document.getElementById("usageMay").value,
+    document.getElementById("usageJun").value,
+    document.getElementById("usageJul").value,
+    document.getElementById("usageAug").value,
+    document.getElementById("usageSep").value,
+    document.getElementById("usageOct").value,
+    document.getElementById("usageNov").value,
+    document.getElementById("usageDec").value,
+  ];
+}
 
-        var totalCost =
-          costJan +
-          costFeb +
-          costMar +
-          costApr +
-          costMay +
-          costJun +
-          costJul +
-          costAug +
-          costSept +
-          costOct +
-          costNov +
-          costDec;
+function getUserAnnualCost() {
+  return getUserMonthlyValues().reduce((a, b) => a + b, 0)
+}
 
-        let allPlansHTMLoutput = `<div class="card">
+usageSubmit.addEventListener("click", async () => {
+  const avaliablePlans = await getPlans(
+    document.getElementById("userZIP").value
+  );
+
+  avaliablePlans.forEach((plan) => {
+    usages.forEach((usage, index) => {
+      //iterate through each month in months
+    });
+
+    let allPlansHTMLoutput = `<div class="card">
                                  <div class="card-body">
                                      <h5 class="card-title">${allPlans.data[index].plan_name}</h5>
                                      <h6 class="card-subtitle mb-2 text-muted">${allPlans.data[index].company_name}</h6>
@@ -104,19 +98,32 @@ usageSubmit.addEventListener("click", function () {
                                      </ul>
                                  </div>
                              </div>`;
-        planResults.innerHTML = allPlansHTMLoutput;
-      }
+    planResults.innerHTML = allPlansHTMLoutput;
+  });
+});
+<div class="card-body">
+  <h5 class="card-title">${allPlans.data[index].plan_name}</h5>
+  <h6 class="card-subtitle mb-2 text-muted">${allPlans.data[index].company_name}</h6>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Total Estimated Cost: $${getUserAnnualCost()}</li>
+    <li class="list-group-item"><a href="${allPlans.data[index].fact_sheet}" target="_blank" rel="noopener noreferrer">Plan Fact Sheet</a></li>
 
-      //   let allPlansHTMLoutput = `<div class="card">
-      //                           <div class="card-body">
-      //                               <h5 class="card-title">${allPlans.data[0].plan_name}</h5>
-      //                               <h6 class="card-subtitle mb-2 text-muted">${allPlans.data[0].company_name}</h6>
-      //                               <ul class="list-group list-group-flush">
-      //                                   <li class="list-group-item"><a href="${allPlans.data[0].fact_sheet}" target="_blank" rel="noopener noreferrer">Plan Fact Sheet</a></li>
+  </ul>
+</div>
+                             </div > `;
+    planResults.innerHTML = allPlansHTMLoutput;
+  });
+});
+                                 <div class="card-body">
+                                     <h5 class="card-title">${allPlans.data[index].plan_name}</h5>
+                                     <h6 class="card-subtitle mb-2 text-muted">${allPlans.data[index].company_name}</h6>
+                                     <ul class="list-group list-group-flush">
+                                     <li class="list-group-item">Total Estimated Cost: $${totalCost}</li>
+                                         <li class="list-group-item"><a href="${allPlans.data[index].fact_sheet}" target="_blank" rel="noopener noreferrer">Plan Fact Sheet</a></li>
 
-      //                               </ul>
-      //                           </div>
-      //                       </div>`;
-      //   planResults.innerHTML = allPlansHTMLoutput;
-    });
+                                     </ul>
+                                 </div>
+                             </div>`;
+planResults.innerHTML = allPlansHTMLoutput;
+  });
 });
